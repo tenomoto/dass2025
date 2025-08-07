c3320591193dec0164719d0fa335c751752d8648
eno <>
2025-08-07 09:51:34 +0000

    add ekf.qmd R/predator-prey_ekf_param.R
    FossilOrigin-Name: 2bb92264c95b5e9b236548ec4e3c374ca43675b1644e10a88ab07883aadb3455

--- R/predator-prey_ekf_param.R
+++ R/predator-prey_ekf_param.R
@@ -17,9 +17,9 @@ tlm <- function(dw, x, y, dt, a, da = rep(0, length(a))) {
   nmax <- length(x)
   for (n in 1:(nmax-1)) {
     dx <- dx + dt * ((a[1] + 2 * a[2] * x[n] + a[3] * y[n]) * dx + a[3] * x[n] * dy +
-      a[1] * x[n] * da[1] + a[2] * x[n]^2 * da[2] + a[3] * x[n] * y[n] * da[3])
+                       a[1] * x[n] * da[1] + a[2] * x[n]^2 * da[2] + a[3] * x[n] * y[n] * da[3])
     dy <- dy + dt * (a[6] * y[n] * dx + (a[4] + 2 * a[5] * y[n] + a[6] * x[n]) * dy +
-      a[4] * y[n] * da[4] + a[5] * y[n]^2 * da[5] + a[6] * x[n] * y[n] * da[6])
+                       a[4] * y[n] * da[4] + a[5] * y[n]^2 * da[5] + a[6] * x[n] * y[n] * da[6])
   }
   c(dx, dy)
 }
@@ -28,7 +28,7 @@ forecast_state <- function(xa, mfunc, ...) {
   mfunc(xa, ...)
 }
 
-forecast_ecov <- function(pamat, tfunc, sq, ...) {
+forecast_ecov <- function(pamat, tfunc, sq, nx, ...) {
   n <- nrow(pamat)
   mpmat <- matrix(0, n, n)
   pfmat <- matrix(0, n, n)
@@ -38,8 +38,10 @@ forecast_ecov <- function(pamat, tfunc, sq, ...) {
   for (i in 1:n) {
     pfmat[, i] <- tfunc(mpmat[i, ], ...)
   }
-  pfmat + matrix(rnorm(n * n, 0, sq), n, n)
-#  pfmat + matrix(diag(rnorm(n, 0, sq)), n, n)
+  qmat <- matrix(0, n, n)
+  qmat[1:nx, 1:nx] <- matrix(rnorm(nx * nx, 0, sq^2), nx, nx)
+#  qmat[1:nx, 1:nx] <- diag(rnorm(nx, 0, sq^2))
+  pfmat + qmat
 }
 
 calc_kgain <- function(pfmat, hmat, rmat) {
@@ -82,27 +84,34 @@ xf_hist <- rep(0, ntobs)
 yf_hist <- rep(0, ntobs)
 xa_hist <- rep(0, ntobs)
 ya_hist <- rep(0, ntobs)
+a_hist <- matrix(0, 0, 6)
+a_hist <- rbind(a_hist, a)
 
 hfunc <- function(x) x
-hmat <- diag(1, 2, 2)
+hmat <- cbind(diag(1, 2, 2), matrix(0, 2, 6))
 sb <- 0.1
 sr <- 0.1
-pamat <- diag(sb^2, 2, 2)
+sa <- 0.1
+pamat <- rbind(cbind(diag(sb^2, 2, 2), matrix(0, 2, 6)),
+               cbind(matrix(0, 6, 2), diag(sa^2, 6, 6)))
 rmat <- diag(sr^2, 2, 2)
 sq <- 0.2
-
+da <- rep(sa, 6)
+#da <- rep(0, 6)
 n <- 1
+#ntobs <- 1
 for (t in 1:ntobs) {
   nf <- tobs[t] - n + 1
   t_hist <- c(t_hist, n:tobs[t], tobs[t])
   xf <- forecast_state(xa, forward, dt = dt, nmax = nf)
-  pfmat <- forecast_ecov(pamat, tlm, sq, x = xf[1,], y = xf[2,], dt = dt, a = a)
+  pfmat <- forecast_ecov(pamat, tlm, sq, nx = 2,
+                         x = xf[1,], y = xf[2,], dt = dt, a = xa[3:8], da = da)
   kmat <- calc_kgain(pfmat, hmat, rmat)
   xa <- analyze_state(xf[, nf], kmat, yo[, t], hfunc)
-  xa <- c(xa, a)
   pamat <- analyze_ecov(pfmat, kmat, hmat, rmat)
   x_hist <- c(x_hist, xf[1, ], xa[1])
   y_hist <- c(y_hist, xf[2, ], xa[2])
+  a_hist <- rbind(a_hist, xa[3:8])
   xf_hist[t] <- xf[1, nf]
   yf_hist[t] <- xf[2, nf]
   xa_hist[t] <- xa[1]
@@ -118,3 +127,26 @@ lines(1:nmax, wt[2,], lwd = 3, lty = 3, col = "red")
 legend("topright", c("x", "y", "xt", "yt"),
        lwd = c(1, 1, 2, 2), lty = c(1, 1, 3, 3),
        col = c("black", "red", "black", "red"))
+
+tobs1 <- c(1, tobs[1:ntobs])
+plot(tobs1, a_hist[, 1], type = "l", lwd = 1, xlab = "t", ylab = "x parameters",
+     ylim = c(-4, 4))
+lines(tobs1, a_hist[, 2], lwd = 1, col = "red")
+lines(tobs1, a_hist[, 3], lwd = 1, col = "blue")
+abline(h = at[1], lwd = 1, lty = 3)
+abline(h = at[2], lwd = 1, lty = 3, col = "red")
+abline(h = at[3], lwd = 1, lty = 3, col = "blue")
+legend("topright", c("a1", "a2", "a3"),
+       lwd = c(1, 1, 1), lty = c(1, 1, 1),
+       col = c("black", "red", "blue"))
+
+plot(tobs1, a_hist[, 4], type = "l", lwd = 1, xlab = "t", ylab = "y parameters",
+     ylim = c(-6, 4))
+lines(tobs1, a_hist[, 5], lwd = 1, col = "red")
+lines(tobs1, a_hist[, 6], lwd = 1, col = "blue")
+abline(h = at[4], lwd = 1, lty = 3)
+abline(h = at[5], lwd = 1, lty = 3, col = "red")
+abline(h = at[6], lwd = 1, lty = 3, col = "blue")
+legend("topright", c("a4", "a5", "a6"),
+       lwd = c(1, 1, 1), lty = c(1, 1, 1),
+       col = c("black", "red", "blue"))
