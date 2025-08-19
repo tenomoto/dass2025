88edfab05992dc27db9338b6631083a3809ac5ca
nakashita <nakashita@noemail.net>
2025-08-19 12:04:58 +0000

    adjoint.f90 is renamed to adjoint_params.f90 & add adjoint.f90 for initial condition estimation
    FossilOrigin-Name: b2ea745eda286e6d193a47ac95c215bc729a21bd982e674ad4911256049bfeb4

--- Fortran/adjoint_params.f90
+++ Fortran/adjoint_params.f90
@@ -1,12 +1,13 @@
-! gfortran adjoint.f90 -o adjoint
-program adjoint_main
+! gfortran adjoint_params.f90 -o adjoint_params
+program adjoint_params_main
   use, intrinsic :: iso_fortran_env, only: dp => real64
   implicit none
 
   logical, parameter :: debug = .true.
   integer, parameter :: &
-    nmax = 501, iobs1 = 2, dobs = 2, maxit = 500, logit=20, un = 51
-  real(dp), parameter :: dt = 0.001_dp, alpha = 1.0d-3, gtol = 1.0d-5
+    nmax = 501, iobs1 = 2, dobs = 2, maxit = 100, un = 51
+  real(dp), parameter :: dt = 0.001_dp, &
+    ctol = 1.0d-10, gtol = 1.0d-5, stol = 1.0e-7
   real(dp), dimension(6), parameter :: &
     at = [4.0_dp, -2.0_dp, -4.0_dp, -6.0_dp, 2.0_dp, 4.0_dp]
   real(dp), parameter :: x1 = 1.0_dp, y1 = 1.0_dp
@@ -16,13 +17,11 @@ program adjoint_main
   real(dp), dimension(nmax) :: x, y, xt, yt, xo, yo, ax, ay
   real(dp), dimension(6) :: a, aa
   real(dp), dimension(8) :: par
-  real(dp) :: gnorm
-  real(dp), dimension(2) :: g, d
   real(dp), dimension(:), allocatable :: hcost, hgnorm
   real(dp), dimension(:, :), allocatable :: hpar
 
   ntobs = (nmax - iobs1) / dobs + 1
-  allocate(tobs(ntobs), hcost(maxit+1), hgnorm(maxit+1), hpar(2, maxit+1))
+  allocate(tobs(ntobs), hcost(maxit+1), hgnorm(maxit+1), hpar(8, maxit+1))
   tobs(:) = [(i, i = iobs1, nmax, dobs)]
 
   a = at
@@ -34,38 +33,17 @@ program adjoint_main
   xo(:) = xt(:)
   yo(:) = yt(:)
   
-  ! initialize control variables
+  a = [1.0_dp, 0.0_dp, 0.0_dp, -1.0_dp, 0.0_dp, 0.0_dp]
   par(1:6) = a(:)
-  par(7:8) = [1.5_dp, 1.5_dp]
-  print *, par
+  par(7:8) = [2.0_dp, 2.0_dp]
 
-  print *, "start optimization"
-  res = 0
-  do iter = 1, maxit
-    ! evaluate cost function and its gradient
-    hcost(iter) = fn(par)
-    g = gr(par)
-    gnorm = sqrt(sum(g**2))
-    hgnorm(iter) = gnorm
-    hpar(:, iter) = par(7:8)
-    if (gnorm < gtol) then
-      print *, "Convergence, Iteration: ", iter
-      print *, "x,y = ", par(7:8)
-      res = 1
-      exit
-    end if
-    if (mod(iter,logit).eq.0) &
-    &  print '(a,i3,2(a,es9.3))', "Iteration:", iter, " Cost:", hcost(iter), " Gradient Norm:", gnorm
-    ! update control variables with steepest descent
-    d = -alpha * g 
-    par(7:8) = par(7:8) + d
-  end do
-  if (res.ne.1) then
-    print *, "reaching maximum iterations"
-    print *, "x,y = ", par(7:8)
-  end if
+  res = bfgs(par)
+  print *, "Convergence: ", res, " Iteration: ", iter
+  print *, "x,y = ", par(7:8)
+  print *, "xparms = ", par(1:3)
+  print *, "yparms = ", par(4:6)
 
-  open(unit = un, file = "out_adjoint.dat", access = "stream", &
+  open(unit = un, file = "out_adjoint_params.dat", access = "stream", &
     form = "unformatted", status = "replace", action = "write")
   write(unit = un) iter, hcost(1:iter), hgnorm(1:iter), hpar(:, 1:iter)
   close(unit = un)
@@ -130,14 +108,14 @@ contains
 
   function gr(par) result(grad)
     real(dp), dimension(8), intent(in) :: par
-    real(dp), dimension(2) :: grad
+    real(dp), dimension(8) :: grad
 
     x(1) = par(7)
     y(1) = par(8)
     a(:) = par(1:6)
     call forward
     call adjoint
-    grad = [ax(1), ay(1)]
+    grad = [aa(1), aa(2), aa(3), aa(4), aa(5), aa(6), ax(1), ay(1)]
 
   end function gr
 
@@ -168,5 +146,68 @@ contains
     end do
 
   end function outer
+    
+  function bfgs(par) result(convergence)
+    real(dp), dimension(8), intent(inout) :: par
+ 
+    integer :: n, convergence
+    real(dp) :: alpha, ys, rho, gnorm
+    real(dp), dimension(size(par)) :: g, new_par, s, y, new_gr
+    real(dp), dimension(:, :), allocatable :: H
 
-end program adjoint_main
+    n = size(par)
+    allocate(H(n, n))
+    H = diag(n)
+    g = gr(par)
+    alpha = min(1.0_dp, 1.0_dp / sum(abs(g)))
+    new_par = par - alpha * g
+    s = -alpha * g
+    y = gr(new_par) - g
+    ys = sum(y * s)
+    H = ys / sum(y * y) * diag(n)
+    if (debug) print *, "Initial Hessian:", H
+  
+    do iter = 1, maxit
+      hcost(iter) = fn(par)
+      gnorm = sqrt(sum(g**2))
+      hgnorm(iter) = gnorm
+      hpar(:, iter) = par(:)
+      if (debug) print *, &
+        "Iteration:", iter, "Cost:", hcost(iter), "Gradient Norm:", gnorm
+      if (debug) print *, par(1:3)
+      if (debug) print *, par(4:6)
+      if (debug) print *, par(7:8)
+      if (gnorm < gtol) then
+        convergence = 0
+        return
+      end if
+      if (maxval(abs(s)) < stol) then
+        convergence = 1
+        return
+      end if
+
+      new_par = par - matmul(H, g)
+      s = new_par - par
+      new_gr = gr(new_par)
+      y = new_gr - g
+      ys = sum(y * s)
+      if (ys > ctol) then
+        rho = 1 / ys
+        H = matmul((diag(n) - rho * outer(s, y)), matmul(H, (diag(n) - rho * outer(y, s)))) &
+            + rho * outer(s, s)
+!        if (debug) print *, "Updated Hessian:", H
+      else
+        H = ys / sum(y * y) * diag(n)
+!        if (debug) print *, "Reset Hessian:", H
+      end if
+      if (iter == maxit) convergence = 2
+    
+      par(:) = new_par(:)
+      g = new_gr
+    end do
+
+    deallocate(H)
+  
+  end function bfgs
+
+end program adjoint_params_main
